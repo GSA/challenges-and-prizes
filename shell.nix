6abf1465d4e4f517506c3a01253e8de71c600f30
Stephen Chudleigh <stephen@smartlogic.io>
2025-01-17 09:28:49 -0800

    update local dev configs

---
+++ shell.nix
@@ -0,0 +1,41 @@
+{
+  lib ? import <lib> {},
+  pkgs ? import (fetchTarball channel:nixos-24.05) {}
+}:
+
+let
+
+
+  # define packages to install with special handling for OSX
+  basePackages = [
+    pkgs.gnumake
+    pkgs.gcc
+    pkgs.readline
+    pkgs.zlib
+    pkgs.libxml2
+    pkgs.libiconv
+    pkgs.openssl
+    pkgs.git
+    pkgs.python3
+
+    pkgs.nodejs_18
+    pkgs.yarn
+    pkgs.ruby_3_1
+    pkgs.license_finder
+
+    pkgs.gh
+    pkgs.ripgrep
+    pkgs.jq
+  ];
+
+  inputs = basePackages
+    ++ [ pkgs.bashInteractive ]
+    ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.inotify-tools ]
+    ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
+        CoreFoundation
+        CoreServices
+      ]);
+
+in pkgs.mkShell {
+  buildInputs = inputs;
+}
